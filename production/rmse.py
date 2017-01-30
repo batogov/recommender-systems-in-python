9a999b33225e0f2f6912cc60f99c3ea917ca4664
batogov <thesiamesecat19@gmail.com>
2017-01-31 00:11:41 +0300

    rmse.py

---
+++ production/rmse.py
@@ -0,0 +1,9 @@
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+import numpy as np
+
+
+def rmse(prediction, ground_truth):
+    prediction = prediction[ground_truth.nonzero()].flatten()
+    ground_truth = ground_truth[ground_truth.nonzero()].flatten()
+    return sqrt(mean_squared_error(prediction, ground_truth))
